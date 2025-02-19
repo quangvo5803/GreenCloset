4f384f1b51ccc11399fa1656be7773c4bed9140e
Võ Văn Quang <quangvo5803@gmail.com>
2025-02-19 08:12:59 +0700

    [QuangVV - 2025/02/19] Create Project

---
+++ GreenCloset/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GreenCloset.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

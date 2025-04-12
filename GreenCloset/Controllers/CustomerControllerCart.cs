5cdfedf76e2041d003d537fead35ed6cbe5489b4
phucphan03 <dinhphucqn44@gmail.com>
2025-04-12 21:34:00 +0700

    no message

---
+++ GreenCloset/Controllers/CustomerControllerCart.cs
@@ -0,0 +1,15 @@
+﻿using BussinessLayer.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenCloset.Controllers
+{
+    public partial class CustomerController : BaseController
+    {
+        public CustomerController(IFacedeService facedeService)
+        : base(facedeService) { }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+    }
+}

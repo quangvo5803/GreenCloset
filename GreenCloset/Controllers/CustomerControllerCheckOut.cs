a0c96a2d5bc3f77e1d0252e76d4c7a4e7220d3be
phucphan03 <dinhphucqn44@gmail.com>
2025-04-18 20:48:42 +0700

    no message

---
+++ GreenCloset/Controllers/CustomerControllerCheckOut.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenCloset.Controllers
+{
+    [Authorize(Roles = "Customer")]
+    public partial class CustomerController : BaseController
+    {
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
